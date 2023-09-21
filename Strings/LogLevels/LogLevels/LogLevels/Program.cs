8d050081f01230cf47ffbf0df00c2babec17b671
James VanMeter <jamesvanmeter1219@gmail.com>
2023-09-20 23:00:33 -0400

    adding methods to LogLevels

--- Strings/LogLevels/LogLevels/LogLevels/Program.cs
+++ Strings/LogLevels/LogLevels/LogLevels/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//https://exercism.org/tracks/csharp/exercises/log-levels
+namespace LogLevels
+{
+    internal class LogLevels
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static string GetMessageFromALogLine()
+        {
+            return "";
+        }
+        
+        public static string GetLogLevelFromALogLine()
+        {
+            return "";
+        }
+
+        public static string ReformatALogLine()
+        {
+            return "";
+        }
+    }
+}
